06ee0c3a8b091349806d475203cd669e44fd60c2
mtijani <mtijani18@yahoo.fr>
2022-06-29 06:10:54 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwGenerator';
+
+  includeLetters = false;
+  includeNumbers=false;
+  includeSymbols=false;
+  password='';
+  length=0;
+  name:string;
+  onChangesLetters(){
+    this.includeLetters=!this.includeLetters;
+  }
+  onChangeNumbers(){
+    this.includeNumbers=!this.includeNumbers;
+  }
+  onChangeSymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+  onChangeLength(value:string){
+    const parsedValue= parseInt(value);
+
+    if(!isNaN(parsedValue)){
+      this.length=parsedValue;
+    }
+    console.log(parsedValue)
+
+
+  }
+
+  onButtonClick(){
+    console.log(this.length)
+
+    const numbers= '1234567890';
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    const symbols='%ù*£$#{(-}|';
+    let validChar='';
+    if(this.includeLetters){
+      validChar+=letters;
+    }
+    if(this.includeNumbers){
+      validChar+=numbers;
+    }
+    if(this.includeSymbols){
+      validChar+=symbols;
+    }
+    let generatedPassword='';
+    for(let i=0;i<this.length;i++){
+      const index = Math.floor(Math.random() * validChar.length);
+      generatedPassword+=validChar[index];
+
+    }
+
+    this.password=generatedPassword;
+
+    console.log(this.password)
+
+  }
+  getPassword(){
+
+    return this.password;
+  }
+
+
 }
